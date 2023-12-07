cb954d18bc7c2f887d9be974edd9895c6f36bfd0
Sonu Panchal <55386781+sonupanchal0606@users.noreply.github.com>
2023-12-07 10:28:26 +0530

    Create a.php
    simple php form to connect with mysql database

---
+++ a.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+<form action="b.php" method="post">
+Account Number: <input type="text" name="accnum"><br>
+Name: <input type="text" name="name"><br>
+Branch Name: <input type="text" name="bname"><br>
+Balance: <input type="text" name="balance"><br>
+<input  type='submit' name = 'a'    value = 'insert'>
+<input  type='submit' name = 'a'    value = 'update'>
+<input  type='submit' name = 'a'    value = 'delete'>
+</form>
+
+</body>
+</html> 
